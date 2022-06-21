03bbf40b59dbf0edf6d881bac4b801c07a147c35
konstantin <kostka009@yandex.ru>
2022-06-21 21:58:40 +0300

    fff

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-// const cors = require('cors');
-const { port, mongoAdress } = require('./utils/constanta');
-
+const cors = require('cors');
+const { port, mongoAdress, allowedCors } = require('./utils/constanta');
+const rateLimiter = require('./middlewares/rate-limit');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errors');
 const router = require('./routes/index');
@@ -19,30 +19,13 @@ const { PORT = port, MONGO_ADRESS, NODE_ENV } = process.env;
 
 mongoose.connect(NODE_ENV === 'production' ? MONGO_ADRESS : mongoAdress);
 
-const cors = (req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-  res.header('Access-Control-Allow-Origin', origin);
-  res.header('Access-Control-Allow-Credentials', true);
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-
-    return res.end();
-  }
-
-  return next();
-};
-
-app.use(cors);
-
 app.use(requestLogger);
-// app.use(rateLimiter);
-
+app.use(rateLimiter);
+app.use(cors({
+  option: allowedCors,
+  origin: allowedCors,
+  credentials: true,
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
@@ -63,7 +46,5 @@ app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(
-    `App listening on port ${PORT}`,
-  ); /* eslint-disable-line no-console */
+  console.log(`App listening on port ${PORT}`); /* eslint-disable-line no-console */
 });
