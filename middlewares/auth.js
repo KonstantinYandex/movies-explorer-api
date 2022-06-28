b4344b1ae7b7a19844b2bef218c1729b905a472c
konstantin <kostka009@yandex.ru>
2022-06-28 22:35:47 +0300

    first

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,27 +1,23 @@
 const jwt = require('jsonwebtoken');
+
 const NotAuthError = require('../errors/not-auth-error');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+  const token = req.cookies.jwt;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new NotAuthError('Необходима авторизация'));
+  if (!token) {
+    next(new NotAuthError('К этому ресурсу есть доступ только для авторизированных пользователей'));
   }
 
-  const token = authorization.replace('Bearer ', '');
-
   let payload;
-
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return next(new NotAuthError('Необходима авторизация'));
+    throw new NotAuthError('Необходима авторизация');
   }
-
   req.user = payload;
 
-  next();
+  return next();
 };
