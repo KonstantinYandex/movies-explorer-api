295e857890dc77e6568348c72a3e4d97c8905990
konstantin <kostka009@yandex.ru>
2022-07-08 14:23:06 +0300

    first

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const { port, mongoAdress, allowedCors } = require('./utils/constanta');
+// const cors = require('cors');
 const rateLimiter = require('./middlewares/rate-limit');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errors');
@@ -15,17 +14,36 @@ const router = require('./routes/index');
 
 const app = express();
 
-const { PORT = port, MONGO_ADRESS, NODE_ENV } = process.env;
+const { PORT = 3000 } = process.env;
 
-mongoose.connect(NODE_ENV === 'production' ? MONGO_ADRESS : mongoAdress);
+mongoose.connect('mongodb://localhost:27017/moviesdb', {
+  useNewUrlParser: true,
+});
 
 app.use(requestLogger);
 app.use(rateLimiter);
-app.use(cors({
-  option: allowedCors,
-  origin: allowedCors,
-  credentials: true,
-}));
+const cors = (req, res, next) => {
+  const { origin } = req.headers;
+  console.log(origin);
+  console.log('11111');
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Credentials', true);
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    return res.end();
+  }
+
+  return next();
+};
+
+app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
